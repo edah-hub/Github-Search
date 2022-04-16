fa7f5e7c43f261d03c67c06363e345a353c35f3d
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-16 07:43:04 +0300

    adds a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsComponent } from './components/footer/components.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentsComponent
+    ComponentsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
