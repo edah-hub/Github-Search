a4b2bc69ed708c42c6ccc32cd58a5a85c6175ac5
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-16 07:40:33 +0300

    edits the component name

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ComponentsComponent } from './github-app/components/components.component';
+import { ComponentsComponent } from './components/footer/components.component';
 
 @NgModule({
   declarations: [
