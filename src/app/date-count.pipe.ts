c67ab36362fc217781bb7457def1228125116af5
edah-hub <edah.chepngetich@student.moringaschool.comm>
2022-04-17 07:26:09 +0300

    updates the pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: any): number {
+    let today:Date = new Date();
+    let todayWitNoTime:any = new Date (today.getFullYear(),today.getMonth(),today.getDate())
+    var dateDifference = Math.abs(todayWitNoTime-value)
+    const secondsInDay = 86400;
+    var dateDifferenceSeconds = dateDifference*0.001;
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+    
+    if(dateCounter>=1 && value > todayWitNoTime){
+      return dateCounter
+    } else {
+      return 0;
+    }
+   
   }
 
 }
